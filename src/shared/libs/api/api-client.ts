427a876088ea5570474fba6298effbdf7cc8ef5d
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-03-04 23:48:01 +0300

    feat: универсальную функцию для отправки запросов с авторизацией

---
+++ src/shared/libs/api/api-client.ts
@@ -0,0 +1,65 @@
+import { IUser } from '@shared/libs/interfaces';
+import { BASE_SERVER_URL } from '@shared/libs/utils/constants';
+import { useAuthStore } from '@shared/store/authStore';
+
+interface AuthResponse {
+	user: IUser;
+	accessToken: string;
+}
+
+export const getNewRefreshToken = async (): Promise<AuthResponse> => {
+	const response = await fetch(`${BASE_SERVER_URL}/auth/login/access-token`, {
+		method: 'POST',
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+
+	if (!response.ok) {
+		throw new Error('Failed to refresh token');
+	}
+
+	return response.json();
+};
+
+export const fetchWithAuth = async <T>(input: RequestInfo, init?: RequestInit): Promise<T> => {
+	const { accessToken, setAccessToken, clearAuth } = useAuthStore.getState();
+
+	const headers = new Headers(init?.headers);
+	if (accessToken) {
+		headers.append('Authorization', `Bearer ${accessToken}`);
+	}
+
+	let response: Response;
+
+	try {
+		response = await fetch(input, {
+			...init,
+			headers
+		});
+
+		if (response.status === 401) {
+			const { accessToken } = await getNewRefreshToken();
+			setAccessToken(accessToken);
+
+			headers.set('Authorization', `Bearer ${accessToken}`);
+			response = await fetch(input, {
+				...init,
+				headers
+			});
+
+			if (!response.ok) {
+				throw new Error('Request failed after token refresh');
+			}
+		}
+	} catch (error) {
+		clearAuth();
+		if (error instanceof Error) {
+			console.error(`Fetch error: ${error.message}`);
+		}
+		throw error;
+	}
+
+	return (await response.json()) as Promise<T>;
+};
