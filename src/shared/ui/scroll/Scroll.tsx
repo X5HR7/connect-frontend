335bfa402b173e60c1777cc80d02544d3372afae
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-03-09 21:30:10 +0300

    feat: сделать кастомный элемент прокрутки с возможностью динамической подгрузки данных

---
+++ src/shared/ui/scroll/Scroll.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './Scroll.module.scss';
+
+interface CustomScrollProps {
+	children: React.ReactNode;
+	onLoadMore: () => void;
+	hasMore: boolean;
+	loading: boolean;
+}
+
+const Scroll: React.FC<CustomScrollProps> = ({ children, onLoadMore, hasMore, loading }) => {
+	const contentRef = useRef<HTMLDivElement>(null);
+	const scrollTrackRef = useRef<HTMLDivElement>(null);
+	const scrollThumbRef = useRef<HTMLDivElement>(null);
+	const [thumbHeight, setThumbHeight] = useState(20);
+	const [scrollStartPosition, setScrollStartPosition] = useState<number | null>(null);
+	const [initialScrollTop, setInitialScrollTop] = useState<number>(0);
+	const [isDragging, setIsDragging] = useState(false);
+
+	useEffect(() => {
+		const contentElement = contentRef.current;
+		if (contentElement) {
+			const { scrollHeight, clientHeight } = contentElement;
+			const newThumbHeight = (clientHeight / scrollHeight) * clientHeight;
+			setThumbHeight(newThumbHeight);
+		}
+	}, [children]);
+
+	const handleScroll = () => {
+		const contentElement = contentRef.current;
+		const scrollTrackElement = scrollTrackRef.current;
+		const scrollThumbElement = scrollThumbRef.current;
+
+		if (!contentElement || !scrollTrackElement || !scrollThumbElement) return;
+
+		const { scrollTop, scrollHeight, clientHeight } = contentElement;
+		const trackHeight = scrollTrackElement.clientHeight;
+
+		const newTop = (scrollTop / scrollHeight) * trackHeight;
+		scrollThumbElement.style.top = `${newTop}px`;
+
+		if (scrollTop + clientHeight >= scrollHeight - 20 && hasMore && !loading) {
+			onLoadMore();
+		}
+	};
+
+	const handleThumbMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.preventDefault();
+		setScrollStartPosition(e.clientY);
+		if (contentRef.current) {
+			setInitialScrollTop(contentRef.current.scrollTop);
+		}
+		setIsDragging(true);
+	};
+
+	const handleThumbMouseMove = (e: MouseEvent) => {
+		if (isDragging && contentRef.current && scrollStartPosition !== null) {
+			const deltaY = (e.clientY - scrollStartPosition) * 2;
+			contentRef.current.scrollTop = initialScrollTop + deltaY;
+		}
+	};
+
+	const handleThumbMouseUp = () => {
+		setIsDragging(false);
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousemove', handleThumbMouseMove);
+		document.addEventListener('mouseup', handleThumbMouseUp);
+		return () => {
+			document.removeEventListener('mousemove', handleThumbMouseMove);
+			document.removeEventListener('mouseup', handleThumbMouseUp);
+		};
+	}, [isDragging]);
+
+	return (
+		<div className={styles.scroll}>
+			<div ref={contentRef} className={styles.scroll__container} onScroll={handleScroll}>
+				{children}
+				{loading && <div>Загрузка...</div>}
+			</div>
+
+			<div ref={scrollTrackRef} className={styles.scroll__bar}>
+				<div
+					ref={scrollThumbRef}
+					style={{
+						height: `${thumbHeight}px`
+					}}
+					className={styles.scroll__element}
+					onMouseDown={handleThumbMouseDown}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export { Scroll };
