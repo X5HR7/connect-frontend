4651ca665481c9b8a8eafe94181ff611928a9a1c
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-03-09 17:51:00 +0300

    feat: реализовать Tooltip для добовления подсказок при наведении на компонент

---
+++ src/shared/ui/tooltip/Tooltip.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from 'react';
+import styles from './Tooltip.module.scss';
+
+interface ITooltipProps {
+	text: string;
+	children: ReactNode;
+	position?: 'top' | 'right';
+}
+
+const Tooltip: FC<ITooltipProps> = ({ position, text, children }) => {
+	return (
+		<div className={styles.tooltip}>
+			{children}
+			<span className={`${styles.toolpit__text} ${styles[`tooltip__text_${position}`]}`}>{text}</span>
+		</div>
+	);
+};
+
+export { Tooltip };
