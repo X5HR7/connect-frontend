e6f0599613361f35bcaf79bf0a8b6f6cf0d1d7f6
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-03-10 18:58:31 +0300

    feat: реализовать функцию для отображения статуса пользователя в корректном виде

---
+++ src/shared/libs/utils/getUserStatus.ts
@@ -0,0 +1,14 @@
+import { TProfileStatus } from '@shared/libs/interfaces/user.interface.ts';
+
+export const getUserStatus = (status: TProfileStatus) => {
+	switch (status) {
+		case 'ONLINE':
+			return 'В сети';
+		case 'INACTIVE':
+			return 'Неактивен';
+		case 'DO_NOT_DISTURB':
+			return 'Не беспокоить';
+		case 'OFFLINE':
+			return 'Не в сети';
+	}
+};
