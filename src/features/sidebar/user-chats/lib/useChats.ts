8811da4b66f38cd8cdbbaea84c12c410686e242f
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-03-12 16:24:04 +0300

    refactor: переименовать файл user-friends в user-chats

--- src/features/sidebar/user-chats/lib/useChats.ts
+++ src/features/sidebar/user-chats/lib/useChats.ts
@@ -1,4 +1,4 @@
-import { fetchUserChats } from '@shared/libs/api/friends/user-friends.ts';
+import { fetchUserChats } from '@shared/libs/api/friends/user-chats.ts';
 import { IChat } from '@shared/libs/interfaces';
 import { useAuthStore } from '@shared/store/authStore.ts';
 import { useQuery } from '@tanstack/react-query';
