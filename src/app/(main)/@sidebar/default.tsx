ad0a43989b77920b0f852a438c4660ae1c730753
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-03-18 18:38:03 +0300

    refactor: использовать переменные url из url.config

--- src/app/(main)/@sidebar/default.tsx
+++ src/app/(main)/@sidebar/default.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { urls } from '@shared/libs/utils/url.config.ts';
 import dynamic from 'next/dynamic';
 import { usePathname } from 'next/navigation';
 
@@ -8,10 +9,13 @@ const FriendsSidebar = dynamic(() => import('@widgets/friends-sidebar'));
 
 const Sidebar = () => {
 	const path = usePathname();
-	if (path.startsWith('/server')) {
+
+	if (path.startsWith(urls.SERVERS)) {
 		return <ServerSidebar />;
-	} else {
+	} else if (path.startsWith(urls.FRIENDS)) {
 		return <FriendsSidebar />;
+	} else {
+		return null;
 	}
 };
 
