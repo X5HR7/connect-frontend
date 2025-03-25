7259ff278c92d2df6267f54c5d8a8a9a842f8876
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-03-25 18:09:49 +0300

    refactor: перенести стили 404 страницы в globals.scss

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,6 @@
 import { APP_NAME } from '@shared/libs/utils/constants.ts';
 import { GoBackButton } from '@shared/ui/go-back-button/GoBackButton.tsx';
 import { Metadata } from 'next';
-import styles from './not-found.module.scss';
 
 export const metadata: Metadata = {
 	title: `${APP_NAME} | 404`
@@ -9,10 +8,10 @@ export const metadata: Metadata = {
 
 const NotFound = () => {
 	return (
-		<div className={styles.page}>
-			<div className={styles.page__content}>
-				<h1 className={styles['page__content-title']}>Ошибка 404!</h1>
-				<p className={styles['page__content-text']}>
+		<div className={'not-found-page'}>
+			<div className={'not-found-page__content'}>
+				<h1 className={'not-found-page__content-title'}>Ошибка 404!</h1>
+				<p className={'not-found-page__content-text'}>
 					Запрашиваемая страница не найдена. Возможно вы перешли по некорректному URL или страница была перемещена.
 				</p>
 				<GoBackButton />
