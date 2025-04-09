afd6d5d0cb9b23b553c93f5fb5321681bfef42ea
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-04-10 00:41:42 +0300

    feat: реализовать модальное окно на странице чата (лс) для отображения закрепленных сообщений

--- src/features/chat/pinned-messages-button/ui/PinnedMessagesButton.tsx
+++ src/features/chat/pinned-messages-button/ui/PinnedMessagesButton.tsx
@@ -1,20 +1,65 @@
 'use client';
 
+import { UserMessage } from '@entities/chat/message';
 import pinnedIcon from '@shared/assets/icons/pinned.svg';
+import { IMessage } from '@shared/libs/interfaces';
+import { useAuthStore } from '@shared/store/authStore.ts';
+import { useChatStore } from '@shared/store/chatStore.ts';
+import { Scroll } from '@shared/ui/scroll/Scroll.tsx';
 import { Tooltip } from '@shared/ui/tooltip/Tooltip.tsx';
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from './PinnedMessagesButton.module.scss';
 
 const PinnedMessagesButton: FC = () => {
-	const handleButtonClick = () => {};
+	const { user } = useAuthStore();
+	const { messages, receiver } = useChatStore();
+	const [filteredMessages, setFilteredMessages] = useState<IMessage[]>([]);
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (messages.length) {
+			setFilteredMessages(messages.filter(message => message.isPinned));
+		} else {
+			setFilteredMessages([]);
+		}
+	}, [messages]);
+
+	const handleButtonClick = () => {
+		setIsModalOpen(prev => !prev);
+	};
 
 	return (
-		<button className={styles.button} onClick={handleButtonClick}>
-			<Tooltip text={'Запрепленные сообщения'} position={'bottom'}>
-				<Image src={pinnedIcon} alt={'Pinned'} className={styles.button__image} />
-			</Tooltip>
-		</button>
+		<div className={styles.wrapper}>
+			<button className={styles.button} onClick={handleButtonClick}>
+				<Tooltip text={'Запрепленные сообщения'} position={'bottom'}>
+					<Image src={pinnedIcon} alt={'Pinned'} className={styles.button__image} />
+				</Tooltip>
+			</button>
+			<div className={`${styles.modal} ${isModalOpen ? '' : styles.modal_hidden}`}>
+				<h2 className={styles.modal__title}>Закреплённые сообщения</h2>
+				{filteredMessages.length === 0 ? (
+					<p className={styles.modal__placeholder}>В этом чате нет закрепленных сообщений.</p>
+				) : (
+					<ul className={styles.modal__messages}>
+						<Scroll>
+							{filteredMessages.map(message => {
+								if (user && receiver) {
+									return (
+										<UserMessage
+											key={message.id}
+											message={message}
+											sender={message.userId === receiver.id ? receiver?.member : user}
+										/>
+									);
+								}
+								return null;
+							})}
+						</Scroll>
+					</ul>
+				)}
+			</div>
+		</div>
 	);
 };
 
