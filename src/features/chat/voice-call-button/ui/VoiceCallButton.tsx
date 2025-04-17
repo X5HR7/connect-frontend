db1977d50cfe8baf139e7e38707a34616b85b877
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-04-17 21:31:15 +0300

    refactor: показывать сообщение-заглушку при попытке начать звонок

--- src/features/chat/voice-call-button/ui/VoiceCallButton.tsx
+++ src/features/chat/voice-call-button/ui/VoiceCallButton.tsx
@@ -1,13 +1,22 @@
 'use client';
 
 import callIcon from '@shared/assets/icons/voice_call.svg';
+import { useModalStore } from '@shared/store/modalStore.ts';
 import { Tooltip } from '@shared/ui/tooltip/Tooltip.tsx';
 import Image from 'next/image';
 import { FC } from 'react';
 import styles from './VoiceCallButton.module.scss';
 
 const VoiceCallButton: FC = () => {
-	const handleButtonClick = () => {};
+	const { openModal } = useModalStore();
+
+	const handleButtonClick = () => {
+		openModal(
+			<div className={styles.modal}>
+				Ошибка подключения. Проверьте свое Интернет соединение или повторите попытку позже.
+			</div>
+		);
+	};
 
 	return (
 		<button className={styles.button} onClick={handleButtonClick}>
