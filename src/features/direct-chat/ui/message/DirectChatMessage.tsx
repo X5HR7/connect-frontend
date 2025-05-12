bcd3f4de7923cd4e6bed8fe81d0219d4686da557
Goncharuk Alexander <a.d.goncharuk@gmail.com>
2025-05-12 13:33:14 +0300

    feat: реализовать открытие модального окна с профилем пользователя при клике на автора сообщения в личных чатах

--- src/features/direct-chat/ui/message/DirectChatMessage.tsx
+++ src/features/direct-chat/ui/message/DirectChatMessage.tsx
@@ -3,12 +3,18 @@
 import dynamic from 'next/dynamic';
 import { FC, memo } from 'react';
 import { MessageLayout } from '@entities/message';
+import { useModalStore } from '@shared/store/modalStore.ts';
 import { DirectChatMessageProps } from '../../lib/direct-chat-message.inteface.ts';
 
+const ModalUserProfile = dynamic(() => import('@features/modal-user-profile'));
 const MessageControls = dynamic(() => import('../controls/MessageControls.tsx'));
 
 const DirectChatMessage: FC<DirectChatMessageProps> = ({ message, sender, parentMessageSender }) => {
-	const handleUsernameClick = () => {};
+	const openModal = useModalStore(state => state.openModal);
+
+	const handleUsernameClick = () => {
+		openModal(<ModalUserProfile userId={sender.id} />);
+	};
 
 	return (
 		<MessageLayout
